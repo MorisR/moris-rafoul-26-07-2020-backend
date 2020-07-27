3e39cdaaa43e7be4aea2b4e934ac751f037706f5
moris <moris.rafol@gmail.com>
2020-07-27 14:46:12 +0300

    feat: closes #14 - add choose database ssl through the .env file

--- src/database/postgres/dbConnection.js
+++ src/database/postgres/dbConnection.js
@@ -4,18 +4,20 @@ require("dotenv").config()
 
 
 let connectionString = process.env.DB_POSTGRES_URL;
-
+let ssl = process.env.DB_POSTGRES_URL_SSL || ""
 
 if(!inProduction)
+{
     connectionString = process.env.DB_POSTGRES_TEST_URL;
+    ssl = process.env.DB_POSTGRES_TEST_URL_SSL || "";
+}
+
+
+ssl = ssl.toLowerCase() === "true";
 
 
 if(!connectionString)
     throw new Error('Env variable DB_URL - DB_TESTING_URL must be set');
 
 
-module.exports = new Pool({
-    connectionString: connectionString,
-    ssl: inProduction
-
-})
+module.exports = new Pool({connectionString, ssl })
