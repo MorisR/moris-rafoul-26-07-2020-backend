e8568beb162db587a53d7f5ebe4daa13944c12c2
moris <moris.rafol@gmail.com>
2020-07-29 02:39:15 +0300

    feat: add home route and favicon closes #4, closes #3

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 //express dependencies
 const express = require("express")
-const favicon = require('serve-favicon');
+const serveFavicon = require('serve-favicon');
+const path = require('path');
 const helmet = require("helmet")
 const cookieParser = require("cookie-parser")
 const cookieSession = require('cookie-session')
@@ -13,7 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser())
 app.use(helmet())
 app.disable('x-powered-by');
-
+app.use(serveFavicon(path.join(__dirname,"..","public","favicon.png")))
 
 const sessionSecret = getAndRequireEnvVar("COOKIE_SESSION_SECRET");
 app.use(cookieSession({
@@ -22,10 +23,10 @@ app.use(cookieSession({
 }))
 
 
-
-
+app.get("/",(req,res)=>
+    res.send(`<h1>welcome to the backend!😊😎</h1>
+              <h3>more info on how to use the api could be found <a href='https://github.com/MorisR/moris-rafoul-26-07-2020'>here</a>!</h3>`))
 app.use(router)
-
 
 
 module.exports = app;
