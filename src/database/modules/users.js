7f0fc9bf1489125e68c3cf0fb411fc733fbbbc21
moris <moris.rafol@gmail.com>
2020-07-28 15:48:11 +0300

    users module modifications feat: limit users module outputs to not return all the fields feat: add validateCredentials function

--- src/database/modules/users.js
+++ src/database/modules/users.js
@@ -1,32 +1,59 @@
 const {query: {users}} = require("../postgres")
-const {validateInputs,usersSchema} = require("../../util/inputValidators");
-
+const {validateInputs, usersSchema} = require("../../util/inputValidators");
 
 
 exports.get = async ({userId, email} = {}) => {
 
-    validateInputs( {userId, email}, usersSchema.get)
+    validateInputs({userId, email}, usersSchema.get)
 
     if (!email && !userId)
         throw new Error("user id or email address must be provided")
 
-    return await users.get({userId, email})
+    const userData = await users.get({userId, email})
+    return filterUserData(userData)
 }
 exports.update = async (userId, fieldsToUpdate) => {
 
-    validateInputs( {userId, fieldsToUpdate}, usersSchema.update)
-    return await users.update(userId, fieldsToUpdate)
+    validateInputs({userId, fieldsToUpdate}, usersSchema.update)
+
+    const userData = await users.update(userId, fieldsToUpdate)
+    return filterUserData(userData)
+
 }
 exports.add = async ({email, password, firstName, lastName} = {}) => {
 
-    validateInputs( {email, password, firstName, lastName}, usersSchema.add)
-    return await users.add({email, password, firstName, lastName})
+    validateInputs({email, password, firstName, lastName}, usersSchema.add)
+
+    const userData = await users.add({email, password, firstName, lastName})
+    return filterUserData(userData)
 }
 exports.delete = async (userId) => {
 
-    validateInputs( {userId}, usersSchema.delete)
-    return await users.delete(userId)
-
+    validateInputs({userId}, usersSchema.delete)
+    const userData = await users.delete(userId)
+    return filterUserData(userData)
 }
 
 
+exports.validateCredentials = async (email, password) => {
+    const userData = await exports.get({email})
+    if (!userData)
+        throw new Error("user not found!")
+
+    if (userData.password !== password)
+        throw new Error("incorrect password!")
+
+    return filterUserData(userData)
+}
+
+function filterUserData(userData) {
+
+    return {
+        email: userData.email,
+        firstName: userData.firstName,
+        lastName: userData.lastName,
+        id: userData.id,
+
+    }
+
+}
