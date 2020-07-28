f6c938dd7557715f23568390a8cac6b3ac70f431
moris <moris.rafol@gmail.com>
2020-07-28 15:57:17 +0300

    feat: add routes /auth/login and /auth/currentUser
    closes #24, closes #23

--- src/controllers/router.js
+++ src/controllers/router.js
@@ -2,17 +2,20 @@ const express = require("express")
 const router = express.Router();
 
 const messagesRoutes = require("./routes/messages")
+const authRoutes = require("./routes/auth")
 
 
-router.get("/messages/:messageId", messagesRoutes.getMessage)
 router.get("/messages/received", messagesRoutes.getReceived)
 router.get("/messages/sent", messagesRoutes.getSent)
 router.get("/messages/trash",messagesRoutes.getInTrash)
 router.post("/messages",messagesRoutes.addMessage)
 router.post("/messages/delete/:messageId", messagesRoutes.deleteMessage)
 router.post("/messages/trash/:messageId/:isTrash",messagesRoutes.setTrashState)
+router.get("/messages/:messageId", messagesRoutes.getMessage)
 
 
+router.post("/auth/login",authRoutes.postLogin)
+router.get("/auth/currentUser",authRoutes.getCurrentUserData )
 
 
 
---
+++ src/controllers/routes/auth.js
@@ -0,0 +1,31 @@
+const {users: usersModule} = require("../../database/modules")
+const {send} = require("../../util/serverResponse");
+const sessions = require("../../util/serverSessions");
+
+exports.postLogin = async (req, res) => {
+
+    const {email, password} = req.body;
+
+    try {
+        const userData = await usersModule.validateCredentials(email, password)
+        sessions.createSession(req, {userId: userData.id})
+        send(res, {message: "logged in successfully!"})
+
+    } catch ({message}) {
+        send(res, {message, status: 401})
+    }
+
+}
+exports.getCurrentUserData = async (req, res) => {
+
+    const userId = sessions.getSession(req).userId
+
+    try {
+        const userData =await usersModule.get({userId})
+        send(res, {data:userData})
+
+    } catch ({message}) {
+        send(res, {message, status: 401})
+    }
+
+}
