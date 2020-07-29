d85c46dfcc9cff8eb18e3e4229c2e9f4c1d8425d
moris <moris.rafol@gmail.com>
2020-07-29 04:31:19 +0300

    fix: fix last commit - incorrect string phases placement  closes #33

--- src/controllers/middleware/requireLogin.js
+++ src/controllers/middleware/requireLogin.js
@@ -4,7 +4,7 @@ const sessions = require("../../util/serverSessions");
 module.exports = (req, res, next) => {
 
     if (sessions.hasSessionExpired(req))
-        return send(res, {message: "cannot access route while user logged in", status: 401})
+        return send(res, {message: "log in required in order to precede", status: 401})
 
     else next()
 }
