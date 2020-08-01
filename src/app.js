025c20f59b37afb239d68badacbb117214cdbb29
moris <moris.rafol@gmail.com>
2020-08-02 02:11:01 +0300

    feat: fix last commit - incorrect cors dependency use

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const app = express()
 const router = require("./controllers/router")
 const {getAndRequireEnvVar} = require("./util/envCheck")
 
-app.use(cors)
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser())
