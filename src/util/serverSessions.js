29eec9249c0191e0a1a73597f7e66f9e9f17e9a6
moris <moris.rafol@gmail.com>
2020-08-02 06:54:27 +0300

    Revert "fix: manually reset cookies"
    This reverts commit 4cfa14a4

--- src/util/serverSessions.js
+++ src/util/serverSessions.js
@@ -18,8 +18,6 @@ exports.clearSession = (req, res) => {
     req.session = null
     res.clearCookie("session")
     res.clearCookie("session.sig")
-    res.header("Set-Cookie","session=;session.sig=;")
-
 }
 
 exports.resetSessionAge = (req) => {
