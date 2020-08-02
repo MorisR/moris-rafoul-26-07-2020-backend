e3831f2bb0f5d2b177e2b67815423ad6bf379c9c
moris <moris.rafol@gmail.com>
2020-08-02 05:14:19 +0300

    fix: setup cookies and sessions manually

--- src/controllers/middleware/updateSessionExpDate.js
+++ src/controllers/middleware/updateSessionExpDate.js
@@ -3,7 +3,7 @@ const sessions = require("../../util/serverSessions");
 module.exports = (req, res, next) => {
 
     if (!sessions.hasSessionExpired(req))
-        sessions.resetSessionAge(req)
+        sessions.resetSessionAge(req,res)
 
      next()
 }
